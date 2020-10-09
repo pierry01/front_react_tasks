30ee27680687bdc79ff6cc5481a60a799005ad69
Jean Mendonça <pierrybm@gmail.com>
2020-10-09 20:47:16 +0000

    att tasks and list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import './App.scss';
 import Header from './components/header/Header';
+import Tasks from './components/tasks/Tasks';
+import Container from 'react-bootstrap/Container'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCheckCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faCheckCircle, faTrashAlt)
 
-function App() {
+const App = () => {
   return (
-    <>
+    <div>
       <Header/>
-    </>
+      
+      <Container>
+        <Tasks/>
+      </Container>
+    </div>
   );
 }
 
---
+++ src/components/tasks/list/List.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+const List = () => {
+  return (
+   <div>
+     <Card>
+       <Card.Body>
+         Table...
+       </Card.Body>
+     </Card>
+   </div>
+  );
+}
+
+export default List;
