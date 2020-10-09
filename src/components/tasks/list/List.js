32b280973d60c5b0e3f1191d7bfa7872e114a05a
Jean Mendonça <pierrybm@gmail.com>
2020-10-09 21:09:31 +0000

    Add items to list

--- src/components/tasks/list/List.js
+++ src/components/tasks/list/List.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const List = () => {
+const List = (props) => {
   return (
-   <div>
-     <Card>
-       <Card.Body>
-         Table...
-       </Card.Body>
-     </Card>
-   </div>
+    <div>
+      <Card>
+        <Card.Body>
+          <Table responsive>
+            <tbody>
+              {props.tasks.map((task, index) => {
+                return <tr key={task.id}>
+                  <td className="col-md-10">{task.title}</td>
+                  <td>
+                    { 
+                      task.done === false ? 
+                      <a className="check" href="/">
+                        <FontAwesomeIcon icon="check-circle"/>
+                      </a> : null
+                    }
+                  </td>
+                  <td>
+                    <a className="delete" href="/">
+                      <FontAwesomeIcon icon="trash-alt"/>
+                    </a>
+                  </td>
+                </tr>;
+              })}
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 
