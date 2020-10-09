2545a4c13fae858b10b05a34ab9dbc0b769912dc
Jean Mendonça <pierrybm@gmail.com>
2020-10-09 20:29:46 +0000

    Navbar OK

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React from 'react'
-import logo from './logo.svg'
+import './App.scss';
+import Header from './components/header/Header';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCheckCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
-import './App.css';
 
 library.add(faCheckCircle, faTrashAlt)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+    </>
   );
 }
 
